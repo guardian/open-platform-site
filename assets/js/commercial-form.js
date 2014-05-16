d3cc56bcb0fc873a21b9a832c0eaf9b8c1e1909d
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-05-16 17:38:57 +0100

    Moved javascript to own file

---
+++ assets/js/commercial-form.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	    	var viewModel = {
+
+	    		contactName : ko.observable("Name"),
+	    		email : ko.observable("Email"),
+				productDetails : ko.observable("Name of Product / URL"),
+				company : ko.observable("Company"),
+	
+
+		        postForm : function(formElement) {
+		            var self = this;
+		            contactName = self.contactName();
+		            email = self.email();
+					productDetails = self.productDetails();
+					company = self.company();
+					$.ajax({
+					  type: 'POST',
+					  url: "https://mandrillapp.com/api/1.0/messages/send.json",
+					  data: {
+					    'key': 'to-do',
+					    'message': {
+					      'from_email': email,
+					      'to': [
+					          {
+					            'email': 'to-do',
+					            'name': 'to-do',
+					            'type': 'to'
+					          }	
+					        ],
+					      'autotext': 'true',
+					      'subject': 'New syndication request',
+					      'html': 'Please contact the following company for rights-managed access to Content API Name: ' + contactName + 'Email:' + email +'Product:' + productDetails + 'Company:' + company
+					    }
+					  }
+					 }).done(function(response) {
+					   console.log(response); 
+					});
+
+		        }
+	    	};
+	    
+    	ko.applyBindings(viewModel);
+		});
